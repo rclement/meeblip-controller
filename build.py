5574905b44ed65d16dfeeaa3f59559c410a0d19c
Romain Clement <r.clement88@gmail.com>
2018-11-17 12:17:07 +0100

    Use pluginval to perform automated plugins validation

--- build.py
+++ build.py
@@ -9,16 +9,23 @@ supported_platforms = ['Darwin', 'Windows']
 
 juce_path = os.path.abspath('sdks/juce')
 frut_path = os.path.abspath('sdks/frut')
+pluginval_path = os.path.abspath('sdks/pluginval')
 
+build_dir = 'build'
 build_configs = ['Release']
 build_archs_win = ['Win32', 'x64']
 
 
 def _run(cmd):
-  if platform_name == 'Darwin':
-    subprocess.run([cmd], shell=True)
-  elif platform_name == 'Windows':
-    subprocess.run(cmd, shell=True)
+    rv = None
+
+    if platform_name == 'Darwin':
+        rv = subprocess.run([cmd], shell=True)
+    elif platform_name == 'Windows':
+        rv = subprocess.run(cmd, shell=True)
+
+    if rv is not None:
+        rv.check_returncode()
 
 
 def check_platform():
@@ -54,8 +61,30 @@ def build_frut():
     os.chdir(root_path_rel)
 
 
+def build_pluginval():
+    root_path = os.getcwd()
+
+    pluginval_build_path = os.path.join(pluginval_path, 'install')
+    os.chdir(pluginval_build_path)
+    root_path_rel = os.path.relpath(root_path)
+
+    if platform_name == 'Darwin':
+        plugin_build_bin_name = 'mac_build'
+    elif platform_name == 'Windows':
+        plugin_build_bin_name = 'windows_build.bat'
+
+    pluginval_build_bin_path = os.path.join(
+        pluginval_build_path,
+        plugin_build_bin_name
+    )
+
+    _run(pluginval_build_bin_path)
+
+    os.chdir(root_path_rel)
+
+
 def build_plugin():
-    build_path = os.path.abspath('build')
+    build_path = os.path.abspath(build_dir)
 
     if platform_name == 'Darwin':
         root_path = os.getcwd()
@@ -102,6 +131,39 @@ def build_plugin():
             os.chdir(root_path_rel)
 
 
+def validate_plugins():
+    pluginval_bin_path = None
+    plugin_paths = []
+
+    if platform_name == 'Darwin':
+        pluginval_bin_path = os.path.join(
+            pluginval_path,
+            'bin', 'mac', 'pluginval.app', 'Contents', 'MacOS', 'pluginval'
+        )
+
+        for c in build_configs:
+            plugin_paths += [
+                os.path.join(build_dir, c, 'Meeblip Controller.component'),
+                os.path.join(build_dir, c, 'Meeblip Controller.vst'),
+                os.path.join(build_dir, c, 'Meeblip Controller.vst3'),
+            ]
+    elif platform_name == 'Windows':
+        pluginval_bin_path = os.path.join(
+            pluginval_path,
+            'bin', 'windows', 'pluginval.exe'
+        )
+
+        for c in build_configs:
+            for a in build_archs_win:
+                plugin_paths += [
+                    os.path.join(build_dir, a, c, 'VST', 'Meeblip Controller.dll'),
+                    os.path.join(build_dir, a, c, 'VST3', 'Meeblip Controller.vst3'),
+                ]
+
+    for p in plugin_paths:
+        _run(pluginval_bin_path + ' --strictness-level 5 --validate ' + p)
+
+
 def build_installer():
     if platform_name == 'Darwin':
         _run('packagesbuild -v build/installer/meeblip-controller.pkgproj')
@@ -113,7 +175,9 @@ def build_all():
     check_platform()
     init_dependencies()
     build_frut()
+    build_pluginval()
     build_plugin()
+    validate_plugins()
     build_installer()
 
 
